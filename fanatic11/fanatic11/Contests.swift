af9227c6e09e5162ed7f0758fc51decbcfddb6e6
lakshaychhabra <lakshay.chhabra@hotmail.com>
2018-08-20 12:15:03 +0530

    initalization of pods

---
+++ fanatic11/fanatic11/Contests.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  fanatic11
+//
+//  Created by Lakshay Chhabra on 20/08/18.
+//  Copyright © 2018 App Groceries. All rights reserved.
+//
+
+import UIKit
+
+class Contests: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
