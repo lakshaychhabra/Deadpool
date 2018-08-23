1027e6e298fb8b04be0179d6e2b83ec2acda4f2f
lakshaychhabra <lakshay.chhabra@hotmail.com>
2018-08-23 19:42:24 +0530

    Edited Screens

---
+++ fanatic11/fanatic11/Main Controller/NotificationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NotificationViewController.swift
+//  fanatic11
+//
+//  Created by Lakshay Chhabra on 23/08/18.
+//  Copyright © 2018 App Groceries. All rights reserved.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+}
